7bf73fee9d0363b3a65f9a7009ef6504ad5107a9
plant999 <noobcoder5725@gmail.com>
2022-09-13 00:13:46 +0530

    Added basic db functionality

--- app/src/main/java/com/example/todoApp/MainActivity.kt
+++ app/src/main/java/com/example/todoApp/MainActivity.kt
@@ -1,21 +1,29 @@
 package com.example.todoApp
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mTodoAdapter: TodoAdapter
     private lateinit var mMutableListOfToDos: MutableList<Todo>
+    private lateinit var db: TodoDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mMutableListOfToDos =
-            if (savedInstanceState != null) savedInstanceState.getParcelableArrayList("todolist")!!
-            else mutableListOf()
+        db = Room.databaseBuilder(applicationContext, TodoDatabase::class.java, "tododb").build()
+        mMutableListOfToDos = mutableListOf()
         setContentView(R.layout.activity_main)
         mTodoAdapter = TodoAdapter(mMutableListOfToDos)
+        mTodoAdapter.setDB(db)
 
         rvToDoItem.adapter = mTodoAdapter
         rvToDoItem.layoutManager = LinearLayoutManager(this)
@@ -23,18 +31,39 @@ class MainActivity : AppCompatActivity() {
         btnAddTodo.setOnClickListener {
             val todoTitle = etToDoTitle.text.toString()
             if (todoTitle.isNotEmpty()) {
-                val todo = Todo(todoTitle)
+                val todo = Todo(title = todoTitle)
                 mTodoAdapter.addToDo(todo)
                 etToDoTitle.text.clear()
             }
         }
         btnDeleteTodo.setOnClickListener {
-            mTodoAdapter.doneToDos()
+            mTodoAdapter.doneToDos(window.decorView.rootView)
+        }
+
+        //load async from db and then show using adapter
+        GlobalScope.launch(Dispatchers.IO) {
+            mMutableListOfToDos = db.toDoDao().getAllTodo()
+            withContext(Dispatchers.Main) {
+                for (todo in mMutableListOfToDos) {
+                    Log.i("Anukul", todo.title)
+                }
+                mTodoAdapter.updateList(mMutableListOfToDos)
+            }
         }
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putParcelableArrayList("todolist", ArrayList(mMutableListOfToDos));
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        //load async from db and then show using adapter
+        GlobalScope.launch(Dispatchers.IO) {
+            mMutableListOfToDos = db.toDoDao().getAllTodo()
+            withContext(Dispatchers.Main) {
+                for (todo in mMutableListOfToDos) {
+                    Log.i("Anukul", todo.title)
+                }
+                mTodoAdapter.updateList(mMutableListOfToDos)
+            }
+        }
+
+        super.onConfigurationChanged(newConfig)
     }
 }
--- app/src/main/java/com/example/todoApp/TodoAdapter.kt
+++ app/src/main/java/com/example/todoApp/TodoAdapter.kt
@@ -7,19 +7,52 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_todo.view.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
 
 class TodoAdapter(
     private val todos: MutableList<Todo>
 ) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
 
+    private lateinit var tododb: TodoDatabase
+
+    fun setDB(db: TodoDatabase) {
+        tododb = db
+    }
+
     fun addToDo(todo: Todo) {
         todos.add(todo)
+        GlobalScope.launch(Dispatchers.IO) { tododb.toDoDao().insert(todo) }
         notifyItemInserted(todos.size - 1)
     }
 
-    fun doneToDos() {
-        todos.removeAll { todo ->
-            todo.isChecked
+    // below method is use to update our list of notes.
+    fun updateList(newList: MutableList<Todo>) {
+        // on below line we are clearing
+        // our notes array list
+        todos.clear()
+        // on below line we are adding a
+        // new list to our all notes list.
+        todos.addAll(newList)
+        // on below line we are calling notify data
+        // change method to notify our adapter.
+        notifyDataSetChanged()
+    }
+
+    fun doneToDos(view: View) {
+        val tidList: ArrayList<Todo> = arrayListOf()
+        for (todo in todos) {
+            if (todo.isChecked) {
+                tidList.add(todo)
+                GlobalScope.launch(Dispatchers.IO) {
+                    tododb.toDoDao().delete(todo)
+                }
+            }
+        }
+        for (todo in tidList) {
+            todos.remove(todo)
         }
         notifyDataSetChanged()
     }
